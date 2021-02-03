1291792423fe0acf74a0e8bbc788e7baf09fa749
Maurice Le Cordier <mauricelecordier@gmail.com>
2021-02-03 21:24:31 +0200

    reverted purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   // purge: [],
-  purge: ['./pages/**/*.js', './components/**/*.js', './pages/**/*.jsx', './components/**/*.tsx'],
+  // purge: ['./pages/**/*.js', './components/**/*.js', './pages/**/*.jsx', './components/**/*.tsx'], - add this purge back once styling fixed
+  purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
